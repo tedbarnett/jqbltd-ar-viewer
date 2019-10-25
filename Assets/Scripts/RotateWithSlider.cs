0b9c0cc5412736b8ca31619b1d7d4e2c087d3bd6
Ted Barnett <mail@tedbarnett.com>
2019-10-25 14:48:49 -0700

    Add Unity files
    Added Unity files

---
+++ Assets/Scripts/RotateWithSlider.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateWithSlider : MonoBehaviour
+{
+    // Assign in the inspector
+    private GameObject objectToRotate;
+    public Slider slider;
+
+    // Preserve the original and current orientation
+    private float previousValue;
+
+    void Awake()
+    {
+        // Assign a callback for when this slider changes
+        this.slider.onValueChanged.AddListener(this.OnSliderChanged);
+
+        // And current value
+        this.previousValue = this.slider.value;
+    }
+
+    void OnSliderChanged(float value)
+    {
+        objectToRotate = GameObject.Find("TimeWalkObject");
+        Debug.Log("object: " + objectToRotate);
+
+        // How much we've changed
+        float delta = value - this.previousValue;
+        this.objectToRotate.transform.Rotate(Vector3.down * delta * 360);
+
+
+        Debug.Log("object transform: " + objectToRotate.transform);
+
+        // Set our previous value for the next change
+        this.previousValue = value;
+    }
+}
---
+++ Assets/Scripts/TimeWalkControllerPlaced.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+public class TimeWalkControllerPlaced : MonoBehaviour
+{
+    //define a list where the prefabs will be stored
+    public static List<GameObject> myListObjects = new List<GameObject>();
+    public static int numSpawned = 0;
+    public static int numToSpawn = 3;
+    public static int currentObjectIndex = 0;
+    public static int objectsListLength = 0;
+    public static GameObject currentObject;
+    public static int incrementObject = 0;
+    public static int trackablesTotal = 0;
+    private Text modelNameText;
+    private Text debugText;
+    private string objectNameString;
+    private static GameObject trackables;
+
+
+    void Start()
+    {
+        modelNameText = GameObject.Find("Model Name").GetComponent<Text>();
+        debugText = GameObject.Find("Debug Text").GetComponent<Text>();
+
+        // NOTE: make sure all building prefabs are inside this folder: "Assets/Resources/Prefabs"
+
+        Object[] subListObjects = Resources.LoadAll("Prefabs", typeof(GameObject));
+        foreach (GameObject subListObject in subListObjects)
+        {
+            GameObject lo = (GameObject)subListObject;
+            myListObjects.Add(lo);
+            ++objectsListLength;
+        }
+        GameObject myObj = Instantiate(myListObjects[currentObjectIndex]) as GameObject;
+        myObj.transform.parent = gameObject.transform;
+        objectNameString = myObj.name;
+        objectNameString = objectNameString.Substring(5);
+        modelNameText.text = objectNameString.Replace("(Clone)", "");
+        // modelNameText.text = "Place model below"; // blank name until placed
+        myObj.transform.gameObject.SetActive(true); // hide object at start (not yet placed)
+
+        currentObject = myObj;
+
+        debugText.text = "";
+        //debugText.transform.gameObject.SetActive(false); // hide debugText until there is a message
+
+        //audioData = GetComponent<AudioSource>();
+        //audioData.Play(0);
+    }
+
+    public void NextPrefab(int incrementValue)
+    {
+
+        SpawnNextObject(incrementValue);
+    }
+
+    public void SpawnNextObject(int incrementNumber)
+    {
+        Destroy(currentObject);
+        currentObjectIndex = currentObjectIndex + incrementNumber;
+        if (currentObjectIndex >= objectsListLength)
+        {
+            currentObjectIndex = 0;
+        }
+
+        if (currentObjectIndex < 0)
+        {
+            currentObjectIndex = objectsListLength - 1;
+        }
+
+        GameObject myObj = Instantiate(myListObjects[currentObjectIndex]) as GameObject;
+
+        myObj.transform.gameObject.SetActive(true); // Is this necessary???
+
+        myObj.transform.parent = gameObject.transform;
+
+        objectNameString = myObj.name;
+        objectNameString = objectNameString.Substring(5);
+        modelNameText.text = objectNameString.Replace("(Clone)", "");
+        debugText.text = "New model: " + modelNameText.text;
+        debugText.transform.gameObject.SetActive(true); // show debugText
+
+        // myObj.transform.position = transform.position; // NO: instead we will use the object's default position
+
+        currentObject = myObj;
+        // debugText.text = "currentObjectIndex = " + currentObjectIndex;
+    }
+
+    void Update()
+
+    {
+
+    }
+
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    ARRaycastManager m_RaycastManager;
+
+}
