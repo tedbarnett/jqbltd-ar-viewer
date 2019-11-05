cd3974792c208874cc09575cd8a4e8b36ec66da2
Ted Barnett <mail@tedbarnett.com>
2019-11-04 20:07:15 -0500

    Improved scaling and rotation sliders

--- Assets/Scripts TimeWalk/TimeWalkPlaceOnPlane.cs
+++ Assets/Scripts TimeWalk/TimeWalkPlaceOnPlane.cs
@@ -11,57 +11,46 @@ using UnityEngine.XR.ARSubsystems;
 ///
 /// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
 /// and moved to the hit position.
-/// 
-/// Modified 10/26/2019 by Ted Barnett for TimeWalk.org to enable fast switching between models
-/// 
 /// </summary>
-
 [RequireComponent(typeof(ARRaycastManager))]
-
 public class TimeWalkPlaceOnPlane : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("Instantiates this prefab on a plane at the touch location.")]
     GameObject m_PlacedPrefab;
 
+
     // Assign in the inspector
-    private GameObject timeWalkObject;
+    private GameObject objectToModify;
     public Slider rotationSlider;
     public Slider scaleSlider;
 
     // Preserve the original and current orientation
     private float previousValue;
+    private Text debugText;
+    private Text modelNameText;
 
-    /// <summary>
-    /// The prefab to instantiate on touch.
-    /// </summary>
     public GameObject placedPrefab
     {
         get { return m_PlacedPrefab; }
         set { m_PlacedPrefab = value; }
     }
 
-    /// <summary>
-    /// The object instantiated as a result of a successful raycast intersection with a plane.
-    /// </summary>
+    // The object instantiated as a result of a successful raycast intersection with a plane.
     public GameObject spawnedObject { get; private set; }
 
     void Awake()
     {
         m_RaycastManager = GetComponent<ARRaycastManager>();
+        debugText = GameObject.Find("Debug Text").GetComponent<Text>();
+        modelNameText = GameObject.Find("Model Name").GetComponent<Text>();
 
         // Assign a callback for when the rotation slider changes
-        this.rotationSlider.onValueChanged.AddListener(this.OnRotationSliderChanged);
+        this.rotationSlider.onValueChanged.AddListener(this.OnRotationSliderChanged); // rotation slider callback
+        this.scaleSlider.onValueChanged.AddListener(this.OnScaleSliderChanged); // rotation slider callback
 
         // And current value
         this.previousValue = this.rotationSlider.value;
-
-        // Assign a callback for when the SCALE slider changes
-        this.scaleSlider.onValueChanged.AddListener(this.OnScaleSliderChanged);
-
-        // And current value
-        this.previousValue = this.scaleSlider.value;
-
     }
 
     bool TryGetTouchPosition(out Vector2 touchPosition)
@@ -74,11 +63,11 @@ public class TimeWalkPlaceOnPlane : MonoBehaviour
             return true;
         }
 #else
-        if (Input.touchCount > 0)
-        {
-            touchPosition = Input.GetTouch(0).position;
-            return true;
-        }
+            if (Input.touchCount > 0)
+            {
+                touchPosition = Input.GetTouch(0).position;
+                return true;
+            }
 #endif
 
         touchPosition = default;
@@ -91,33 +80,20 @@ public class TimeWalkPlaceOnPlane : MonoBehaviour
         // Return if clicking in UI area
         Touch touch; // per ARCore example (compare to below)
 
-        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
-        {
-            return; // don't update if just touched or m_Content is null (not set yet)
-        }
-
-        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
-        {
-            // debugText.text = "Clicked in UI";
-            return;
-        }
-
-
-
-
-        if (!TryGetTouchPosition(out Vector2 touchPosition))
-            return;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began) return;
+        if (EventSystem.current.IsPointerOverGameObject(touch.fingerId)) return;
+        if (!TryGetTouchPosition(out Vector2 touchPosition)) return;
 
         if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
         {
-            // Raycast hits are sorted by distance, so the first one
-            // will be the closest hit.
-            var hitPose = s_Hits[0].pose;
+            var hitPose = s_Hits[0].pose; // Raycast hits are sorted by distance, so the first one will be the closest hit.
 
-            if (spawnedObject == null) // if the object has not been spawned yet
+            if (spawnedObject == null)
             {
                 spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
-                timeWalkObject = spawnedObject;
+                objectToModify = spawnedObject;
+                modelNameText.text = ModelNameFix(placedPrefab.name);
+
             }
             else
             {
@@ -129,25 +105,42 @@ public class TimeWalkPlaceOnPlane : MonoBehaviour
 
     void OnRotationSliderChanged(float value)
     {
-        // timeWalkObject = GameObject.Find("TimeWalkObject");
-        Debug.Log("object: " + timeWalkObject);
-
         // How much we've changed
         float delta = value - this.previousValue;
-        this.timeWalkObject.transform.Rotate(Vector3.down * delta * 360);
-
-
-        Debug.Log("object transform: " + timeWalkObject.transform);
+        this.objectToModify.transform.Rotate(Vector3.down * delta * 360);
 
         // Set our previous value for the next change
         this.previousValue = value;
     }
 
+
+
+    // SCALE CHANGE
     void OnScaleSliderChanged(float value)
     {
-        // TO DO: Write scale change code
-        Debug.Log("DEBUG: changing scale of object: " + timeWalkObject.transform);
+        // Set scale based on slider position
+        float scaleValue = ScaleConvert(value);
+        debugText.text = "scaleValue = " + scaleValue;
+        this.objectToModify.transform.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+    }
 
+    // Convert Prefab name to Model String
+    string ModelNameFix (string originalName)
+    {
+        string ret;
+        ret = originalName.Substring(5); // strip off number in front of prefab name
+        ret = ret.Replace("(Clone)", "");
+        return ret;
+    }
+
+
+
+    // Convert Scale Slider to Scale Value
+    float ScaleConvert (float value)
+    {
+        float ret;
+        ret = Mathf.Pow(10, (value - 1));
+        return ret;
     }
 
     static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
