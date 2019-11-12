c024ae39b94f4e202f00e19f0a5031dfd945e114
Ted Barnett <mail@tedbarnett.com>
2019-11-12 15:31:29 -0500

    Made bow helicopter blades rotate

---
+++ Assets/rotateAroundCenter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// From https://forum.unity.com/threads/simple-rotation-script-free.510303/
+
+public class rotateAroundCenter : MonoBehaviour
+{
+    [Range(-1.0f, 1.0f)]
+    public float xForceDirection = 0.0f;
+    [Range(-1.0f, 1.0f)]
+    public float yForceDirection = 0.0f;
+    [Range(-1.0f, 1.0f)]
+    public float zForceDirection = 0.0f;
+
+    public float speedMultiplier = 1;
+
+    public bool worldPivote = false;
+
+    private Space spacePivot = Space.Self;
+
+
+    void Start()
+    {
+        if (worldPivote) spacePivot = Space.World;
+    }
+
+    void Update()
+    {
+        this.transform.Rotate(xForceDirection * speedMultiplier
+                            , yForceDirection * speedMultiplier
+                            , zForceDirection * speedMultiplier
+                            , spacePivot);
+    }
+
+}
